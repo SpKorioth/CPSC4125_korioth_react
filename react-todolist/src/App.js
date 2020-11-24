56340bf4fcdadadd4e2f13a0eaa5b59517e82764
ks-csu <korioth_spencer@columbusstate.edu>
2020-11-24 16:39:40 +0000

    app to class

--- react-todolist/src/App.js
+++ react-todolist/src/App.js
@@ -1,17 +1,19 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './TaskList';
 import TaskList from './TaskList';
 
-function App() {
-  return (
-    <div className="App">
-        <h1>To-do List</h1>
-        <input></input><button>add item</button>
-        <TaskList />
-        <TaskList />
-    </div>
-  );
+class App extends Component {
+  render(){
+        return (
+            <div className="App">
+                <h1>To-do List</h1>
+                <input></input><button>add item</button>
+                <TaskList />
+                <TaskList />
+            </div>
+        );
+    }
 }
 
 export default App;
